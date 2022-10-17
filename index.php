bca5927c6284c5ac49f7cf012051ab836004e012
enweek <114285073+enweek@users.noreply.github.com>
2022-10-17 21:48:02 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> My page </title>
+<link rel="stylesheet" href="mycss.css">
+<link rel="shortcut icon" href="favicon.iso">
+</head>
+<body>
+<div class="mydiv1">
+<?php
+echo "30% процентов от числа 584287532 равно ";
+echo 584287532*0.3;
+?>
+</div>
+</body>
+</html>
